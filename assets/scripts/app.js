86b903c3c55c3307b378a66765b2971b960ef4de
shiverstalker <shiverstalker@gmail.com>
2020-07-09 15:46:51 +0100

    beta version2.1

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,30 +8,36 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
 }
 
 function add() {
+  if (userInput.value != '') {
   const initialResult = currentResult;
-  currentResult = currentResult + parseInt(userInput.value);
+  currentResult += parseInt(userInput.value);
   createAndWriteOutput("+" , initialResult, userInput.value);
+} else {alert("Please input a number")}
 }
 
 function subtract() {
+  if (userInput.value != '') {
   const initialResult = currentResult;
-  currentResult = currentResult - parseInt(userInput.value);
+  currentResult -= parseInt(userInput.value);
   createAndWriteOutput("-" , initialResult, userInput.value);
+} else {alert("Please input a number")}
 }
 
 function multiply() {
+  if (userInput.value != '') {
   const initialResult = currentResult;
-  currentResult = currentResult * parseInt(userInput.value);
+  currentResult *= parseInt(userInput.value);
   createAndWriteOutput("*" , initialResult, userInput.value);
+} else {alert("Please input a number")}
 }
 
 function divide() {
-  if (parseInt(userInput.value) != 0) {
+  if (parseInt(userInput.value) != 0 && userInput.value != '') {
     const initialResult = currentResult;
-    currentResult = currentResult / parseInt(userInput.value);
+    currentResult /= parseInt(userInput.value);
     createAndWriteOutput("/" , initialResult, userInput.value);
   } else {
-    alert("Can't devide by Zero in R");
+    alert("Can't devide by Zero in R or input a number!");
   }
 }
 
