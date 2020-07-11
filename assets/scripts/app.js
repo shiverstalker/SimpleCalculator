8c886a66a721b2e5f85547a5a52e033a2740f7d5
shiverstalker <shiverstalker@gmail.com>
2020-07-11 19:05:19 +0100

    optimized code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,63 +2,72 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
+function calculateResult(calculationType) {
+  if (userInput.value != "") {
+    const initialResult = currentResult;
+    if ((calculationType === 'ADD')) {
+      currentResult += parseInt(userInput.value);
+      operator = "+";
+    } else if ((calculationType === 'SUBTRACT')) {
+      currentResult -= parseInt(userInput.value);
+      operator = "-";
+    } else if ((calculationType === 'MULTIPLY')) {
+      currentResult *= parseInt(userInput.value);
+      operator = "*";
+    } else if ((calculationType === 'DIVIDE')) {
+      if (parseInt(userInput.value) != 0) {
+        currentResult /= parseInt(userInput.value);
+        operator = "/";
+      } else {
+        alert("Can't divie by Zero on R!");
+        return;
+      }
+    }
+    createAndWriteOutput(operator, initialResult, userInput.value);
+    writeToLog(calculationType, initialResult, userInput.value, currentResult);
+  } else {
+    alert("Please input a number");
+  }
+}
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  const calcDescription = `(${resultBeforeCalc}) ${operator} (${calcNumber})`
-  outputResult(currentResult, calcDescription)
+  const calcDescription = `(${resultBeforeCalc}) ${operator} (${calcNumber})`;
+  outputResult(currentResult, calcDescription);
   logEntries.push(calcDescription);
 }
 
-function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
-   const logEntry = {
-     opperation: operationIdentifier,
-     prev: prevResult,
-     number: operationNumber,
-     result: newResult
-   };
-   console.log(logEntry);
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    opperation: operationIdentifier,
+    prev: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  console.log(logEntry);
 }
 
 function add() {
-  if (userInput.value != '') {
-  const initialResult = currentResult;
-  currentResult += parseInt(userInput.value);
-  createAndWriteOutput("+" , initialResult, userInput.value);
-  writeToLog('ADD', initialResult, userInput.value, currentResult);
-} else {alert("Please input a number")}
+  calculateResult('ADD');
 }
 
 function subtract() {
-  if (userInput.value != '') {
-  const initialResult = currentResult;
-  currentResult -= parseInt(userInput.value);
-  createAndWriteOutput("-" , initialResult, userInput.value);
-  writeToLog('SUBTRACT', initialResult, userInput.value, currentResult);
-} else {alert("Please input a number")}
+  calculateResult('SUBTRACT');
 }
 
 function multiply() {
-  if (userInput.value != '') {
-  const initialResult = currentResult;
-  currentResult *= parseInt(userInput.value);
-  createAndWriteOutput("*" , initialResult, userInput.value);
-  writeToLog('MULTIPLY', initialResult, userInput.value, currentResult);
-} else {alert("Please input a number")}
+  calculateResult('MULTIPLY');
 }
 
 function divide() {
-  if (parseInt(userInput.value) != 0 && userInput.value != '') {
-    const initialResult = currentResult;
-    currentResult /= parseInt(userInput.value);
-    createAndWriteOutput("/" , initialResult, userInput.value);
-    writeToLog('DIVIDE', initialResult, userInput.value, currentResult);
-  } else {
-    alert("Can't devide by Zero in R or input a number!");
-  }
+  calculateResult('DIVIDE');
 }
 
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
 multiplyBtn.addEventListener("click", multiply);
 divideBtn.addEventListener("click", divide);
-
